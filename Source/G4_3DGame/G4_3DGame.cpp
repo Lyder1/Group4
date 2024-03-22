2d4c66347c40dd33bcf408fbf1b42fe531b1eb12
Edvard Andreassen <mitori0211@gmail.com>
2024-03-22 13:17:52 +0100

    added new project file

---
+++ Source/G4_3DGame/G4_3DGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "G4_3DGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, G4_3DGame, "G4_3DGame" );
